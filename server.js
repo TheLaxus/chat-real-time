666293b188ad9d082b63fcae84c9fcab44658a33
Lucas <lucashp03@gmail.com>
2020-01-04 12:35:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,25 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));(
+app.set('views', path.join(__dirname, 'public')));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', (req, res) =>{
+app.use('/', (req, res) => {
     res.render('index.html');
 });
+
+let messages = [];
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
 
+    socket.emit('previousMessages', messages);
+
     socket.on('sendMessage', data => {
-        console.log(data);
+      messages.push(data);
+      socket.broadcast.emit('receivedMessage', data);
     });
 });
 
